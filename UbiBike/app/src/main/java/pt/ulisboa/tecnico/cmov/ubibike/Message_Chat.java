be1654071f3e27467af1d08004d747847408d413
Bernardo Marques Graça <bernardo.marquesg@gmail.com>
2016-04-20 14:38:05 +0100

    slb

--- UbiBike/app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/Message_Chat.java
+++ UbiBike/app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/Message_Chat.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import org.apache.commons.io.FileUtils;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
@@ -14,11 +16,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+
+import pt.inesc.termite.wifidirect.sockets.SimWifiP2pSocket;
+import pt.inesc.termite.wifidirect.sockets.SimWifiP2pSocketServer;
 
 public class Message_Chat extends AppCompatActivity {
 
@@ -27,6 +37,11 @@ public class Message_Chat extends AppCompatActivity {
     private ArrayList<String> msgList ;
     private String friendName;
     private Calendar c = Calendar.getInstance();
+    private String ipDevice;
+    private SimWifiP2pSocket mCliSocket = null;
+    private SimWifiP2pSocketServer mSrvSocket = null;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,18 +60,31 @@ public class Message_Chat extends AppCompatActivity {
         itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, msgList);    //inicializa sempre o Adapter com os antigos valores
         itemsList.setAdapter(itemsAdapter);
 
+        //ip of the device that corresponds to this user
+        ipDevice=getIntent().getStringExtra("IP");
+        Toast.makeText(this, ipDevice, Toast.LENGTH_SHORT).show();
+
+        // spawn the chat server background task
+       // new IncommingCommTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
+        new OutgoingCommTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,ipDevice);
+
         Button sendMsg = (Button) findViewById(R.id.chatSendButton);
         assert sendMsg != null;
         sendMsg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
                 EditText newItem = (EditText) findViewById(R.id.messageEdit);
                 String msg = newItem.getText().toString();
 
-                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
                 String formattedDate = df.format(c.getTime());
                 String newMsg=formattedDate + ": " + msg;
-                itemsAdapter.insert(newMsg,0);
+                itemsAdapter.add(newMsg);
+
+                new SendCommTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,newMsg);
 
                 //write the new message on the file related with this user
                 writeItems(friendName);
@@ -67,6 +95,56 @@ public class Message_Chat extends AppCompatActivity {
             }
         });
     }
+
+    public class OutgoingCommTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            //Nothing to do in this method
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                mCliSocket = new SimWifiP2pSocket(params[0], Integer.parseInt(getString(R.string.port)));
+            } catch (UnknownHostException e) {
+                return "Unknown Host:" + e.getMessage();
+            } catch (IOException e) {
+                return "IO error:" + e.getMessage();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //Nothing to do in this method
+        }
+    }
+
+    public class SendCommTask extends AsyncTask<String, String, Void> {
+
+        @Override
+        protected Void doInBackground(String... msg) {
+            try {
+                Log.d("Message", msg[0]);
+                mCliSocket.getOutputStream().write((msg[0] + "\n").getBytes());
+                BufferedReader sockIn = new BufferedReader(new InputStreamReader(mCliSocket.getInputStream()));
+                sockIn.readLine();
+                mCliSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mCliSocket = null;
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            //Nothing to do in this method
+            new OutgoingCommTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,ipDevice);
+        }
+    }
+
 
     //Code to persists items to a file (read and write to a file)
     private void readItems(String filename) {
--- UbiBike/app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/Message_Home.java
+++ UbiBike/app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/Message_Home.java
@@ -1,21 +1,55 @@
 package pt.ulisboa.tecnico.cmov.ubibike;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.os.Messenger;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import pt.inesc.termite.wifidirect.SimWifiP2pBroadcast;
+import pt.inesc.termite.wifidirect.SimWifiP2pDevice;
+import pt.inesc.termite.wifidirect.SimWifiP2pInfo;
+import pt.inesc.termite.wifidirect.SimWifiP2pManager;
+import pt.inesc.termite.wifidirect.SimWifiP2pManager.Channel;
+import pt.inesc.termite.wifidirect.service.SimWifiP2pService;
+import pt.inesc.termite.wifidirect.sockets.SimWifiP2pSocket;
+import pt.inesc.termite.wifidirect.sockets.SimWifiP2pSocketManager;
+import pt.inesc.termite.wifidirect.sockets.SimWifiP2pSocketServer;
+import pt.inesc.termite.wifidirect.SimWifiP2pDeviceList;
+import pt.inesc.termite.wifidirect.SimWifiP2pManager.PeerListListener;
+import pt.inesc.termite.wifidirect.SimWifiP2pManager.GroupInfoListener;
 
-public class Message_Home extends AppCompatActivity {
+public class Message_Home extends AppCompatActivity implements PeerListListener, GroupInfoListener {
 
     private ListView list;
     private ArrayAdapter<String> adapterItems;
     private ArrayList<String> items;
+    private SimWifiP2pBroadcastReceiver mReceiver;
+    private SimWifiP2pManager mManager = null;
+    private Channel mChannel = null;
+    private boolean mBound = false;
+    private Messenger mService = null;
+    private Map<String, String> peersInfo = new HashMap<>();
+    private SimWifiP2pSocketServer mSrvSocket = null;
+    private SimWifiP2pSocket mCliSocket = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +57,29 @@ public class Message_Home extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_message__home);
 
-        items = new ArrayList<String>();
+        items = new ArrayList<>();
         list = (ListView) findViewById(R.id.messagesContainer);
-        adapterItems = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        adapterItems = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         list.setAdapter(adapterItems);
+
+        // initialize the WDSim API
+        SimWifiP2pSocketManager.Init(getApplicationContext());
+
+        // register broadcast receiver
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(SimWifiP2pBroadcast.WIFI_P2P_STATE_CHANGED_ACTION);
+        filter.addAction(SimWifiP2pBroadcast.WIFI_P2P_PEERS_CHANGED_ACTION);
+        filter.addAction(SimWifiP2pBroadcast.WIFI_P2P_NETWORK_MEMBERSHIP_CHANGED_ACTION);
+        filter.addAction(SimWifiP2pBroadcast.WIFI_P2P_GROUP_OWNERSHIP_CHANGED_ACTION);
+        mReceiver = new SimWifiP2pBroadcastReceiver(this);
+        registerReceiver(mReceiver, filter);
+
+        Intent intent = new Intent(this.getApplicationContext(), SimWifiP2pService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        mBound = true;
+
+        // spawn the chat server background task
+        new IncommingCommTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
 
         //Button to go to Search User to send a message activity and expects a returned username
         Button search_user=(Button)findViewById(R.id.searchButton);
@@ -36,11 +89,13 @@ public class Message_Home extends AppCompatActivity {
             public void onClick(View v) {
                 //search all available friends neer me
                 adapterItems.clear();
-                adapterItems.insert("vasco",0);
-                adapterItems.insert("bernardo",0);
-                adapterItems.insert("rui",0);
-
+                if (mBound) {
+                    mManager.requestPeers(mChannel, Message_Home.this);
+                } else {
+                    Toast.makeText(v.getContext(), "Service not bound", Toast.LENGTH_SHORT).show();
+                }
             }
+
         });
 
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -50,9 +105,103 @@ public class Message_Home extends AppCompatActivity {
                 String user=(String) adapter.getItemAtPosition(pos);
                 myIntent.putExtra("ToUsername", user );
                 myIntent.putExtra("Username", getIntent().getStringExtra("Username"));
+                myIntent.putExtra("IP",peersInfo.get(user));
                 startActivity(myIntent);
             }
         });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+        unbindService(mConnection);
+        mBound = false;
+    }
+
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        // callbacks for service binding, passed to bindService()
+
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            mService = new Messenger(service);
+            mManager = new SimWifiP2pManager(mService);
+            mChannel = mManager.initialize(getApplication(), getMainLooper(), null);
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mService = null;
+            mManager = null;
+            mChannel = null;
+            mBound = false;
+        }
+    };
+
+    /*
+	 * Asynctasks implementing message exchange
+	 */
+
+    public class IncommingCommTask extends AsyncTask<Void, String, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            Log.d("Message_Home: ", "IncommingCommTask started (" + this.hashCode() + ").");
+            try {
+                mSrvSocket = new SimWifiP2pSocketServer(Integer.parseInt(getString(R.string.port)));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    SimWifiP2pSocket sock = mSrvSocket.accept();
+                    try {
+                        BufferedReader sockIn = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+                        String st = sockIn.readLine();
+                        publishProgress(st);
+                        sock.getOutputStream().write(("\n").getBytes());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Log.d("Error reading socket:", e.getMessage());
+                    } finally {
+                        sock.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.d("Error socket:", e.getMessage());
+                    break;
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            if(values[0]!=null)
+                Toast.makeText(Message_Home.this, "Recebi: "+ values[0], Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /*
+    * Listeners associated to Termite
+    */
+
+    @Override
+    public void onPeersAvailable(SimWifiP2pDeviceList peers) {
+
+        // compile list of devices in range
+        for (SimWifiP2pDevice device : peers.getDeviceList()) {
+            adapterItems.insert(device.deviceName, 0);
+            peersInfo.put(device.deviceName, device.getVirtIp());
+        }
+    }
+
+    @Override
+    public void onGroupInfoAvailable(SimWifiP2pDeviceList devices, SimWifiP2pInfo groupInfo) {
+        //NOTHING TO DO WITH THIS METHOD
     }
 
     @Override
